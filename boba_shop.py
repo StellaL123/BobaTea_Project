f66ebf96e1cff1e18dfc6c20659a9e94061287d2
StellaL123 <stellali110204@gmail.com>
2020-05-14 09:36:12 +0800

    Menu no longer a global variable, changed format

--- boba_shop.py
+++ boba_shop.py
@@ -1,10 +1,4 @@
 # Make our own boba tea shop
-menu = {
-    "Black Sugar Boba Tea" : 3.50,
-    "Taro Boba Tea" : 4.00,
-    "Thai Milk Tea" : 3.75,
-    "Regular Bubble Tea" : 3.00
-}
 
 # Shopowner Functions
     # 1. Add item to the menu
@@ -46,11 +40,12 @@ def add_item(menu):
     print("This is our current menu: ")
     for tea in menu:
         print(tea)
+
     y_or_n = input("Do you want to add anything to the current menu? (Please answer yes or no): ")
     if y_or_n.upper() == "YES":
         added_tea = input("What would like to add?: ").title()
         new_price = input("What will be the price of " + added_tea + "?: ")
-        if type(added_tea) == int or type(added_tea) == float: #I don't think we need the ""
+        if type(added_tea) == int or type(added_tea) == float:
             print("What you have entered for item is not a word!")
         elif type(new_price) == str:
             print("What you have entered for price is not a number!")
@@ -82,6 +77,7 @@ def add_item(menu):
                             menu[added_tea.title()] = float(new_price)
                             print("OK! We have added " + added_tea + "to the menu." )
                             more = input("Do you want to add more? (Please answer yes or no): ")
+
     elif y_or_n.upper() == "NO":
         print("OK! We will not add anything to the menu")
     else:
@@ -110,33 +106,42 @@ def update_item(menu):
     else:
         print("You have not entered yes or no. Please answer the question again!")
         return update_item(menu)
-    return(menu)
+    return menu
 
 # Customer Functions
     # 1. Order drinks (variable amount of drinks)
     # 2. Function to calculate discounts for the total order
 
-# No limit on number of parameters
+# No limit on number of drinks
 def order_now(menu):
+    '''
+        This function takes the order of the customer
+        Calculate the price of their drinks (with possible discounts)
+        Inputs: Takes menu, a dictionary containing drink names (keys) and prices (values)
+        Outputs: Returns total price, a float, of the customer's order
+    '''
     order_list = []
     print("This is our current menu: ")
     print(menu)
     order = input("What would you like to order?(One drink at a time): ").title()
+
     if order not in menu:
-            print("What you have ordered is not on the menu!")
-            y_or_n = input("Do you want to re-order?(Please answer yes or no): ")
-            if y_or_n.upper() == "YES":
-                print("OK! We will show you the menu again.")
-                return order_now(menu) #Why does this not work
-            elif y_or_n.upper() == "NO":
-                print("OK! Hope you have a nice day :)")
-                exit()
-            else:
-                print("You have entered yes or no. We will just show you the menu again just in case")
-                return order_now(menu)
+        print("What you have ordered is not on the menu!")
+        y_or_n = input("Do you want to re-order?(Please answer yes or no): ")
+        if y_or_n.upper() == "YES":
+            print("OK! We will show you the menu again.")
+            return order_now(menu) #Why does this not work
+        elif y_or_n.upper() == "NO":
+            print("OK! Hope you have a nice day :)")
+            exit()
+        else:
+            print("You have entered yes or no. We will just show you the menu again just in case")
+            return order_now(menu)
+
     order_list.append(order)
     price = menu[order]
     more = input("Do you want anything else? 30% off if you buy 3 or more boba tea and 15% for 2 boba tea (Please answer yes or no): ")
+
     while more.upper() == "YES":
         new_order = input("What would you like to order?(One drink at a time): ")
         if new_order not in menu:
@@ -149,24 +154,29 @@ def order_now(menu):
                 print("OK!")
             else:
                 print("Sorry, I don't quite get that. You did not enter yes or no.")
-        if new_order in menu:
+        else:
             order_list.append(new_order)
             price += menu[new_order]
             more = input("Do you want anything else?(Please answer yes or no): ")
+
     number_of_drinks = 0
+
     for items in order_list:
         number_of_drinks += 1
+
     if number_of_drinks >= 3:
         total_price = price * 0.7
     elif number_of_drinks == 2:
         total_price = price * 0.85
     else:
         total_price = price
+
     print("OK, here's your order:")
     for item in order_list:
         print(item)
     print("and the total price is " + str(total_price))
-    return menu, number_of_drinks, price, total_price
+
+    return total_price
 
 # def price(number_)
 # def buy_items(*argv):
@@ -177,11 +187,19 @@ def order_now(menu):
 # buy_items(1, 2)
 # buy_items()
 def main():
+    menu = {
+        "Black Sugar Boba Tea" : 3.50,
+        "Taro Boba Tea" : 4.00,
+        "Thai Milk Tea" : 3.75,
+        "Regular Bubble Tea" : 3.00
+    }
+
     print("Shopowner perspective")
-    delete_item(menu)
-    add_item(menu)
-    update_item(menu)
+    menu = delete_item(menu)
+    menu = add_item(menu)
+    menu = update_item(menu)
     print("Customer perspective")
-    order_now(menu)
+    total = order_now(menu) # float representing customer total
+    print(total)
 
 main()
